0e69d546670e561e5f2337c35983505450d14826
Daniel Smith <memorysight@gmail.com>
2024-07-21 15:47:49 -0400

    updated with indexHTML&functions

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def get_response_from_ai(human_input):
     Demeter:
     """
     
-    prompt * ProptTemplate(
-        input_variables={"history", "human_input"}
+    prompt = ProptTemplate(
+        input_variables={"history", "human_input"},
         template=template
     )
     
@@ -34,7 +34,49 @@ def get_response_from_ai(human_input):
     output = chatgpt_chain.predict(human_input=human_input)
     
     return output
+
+
+
+from flask import Flask, render_template, request
+
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/send_message', methods="POST")
+def send_message():
+    human_input=request.form['human_input']
+    message = get_response_from_ai(human_input)
+    return message
+
+if __name__ == "__main__":
+    app.run(debug=true)
+
+
+
+    def get_voice_message(message):
+        payload = {
+            "Text": message,
+            "model_id":"eleven_monolingual_v1",
+            "voice_settings": {
+                "stability":0,
+                "similarity_boost":0
+            }
+        }
     
+    headers = {
+        'accept': 'audip/mpeg',
+        'xi-api-key': ELEVEN_LABS_API_KEY,
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.post('https://apli.eleventlabs.io/v1/text-to-speech/21m00Tcm4TlvDq8iKWAM?optimize_streaming_latency=0', json=payload, headers=headers),
+    if response.status_code == 200 and response.content:
+        with open('audio.mp3', 'wb') as f:
+            f.write(response.content)
+        playsound('audio.mp3')                     
     
     
     
